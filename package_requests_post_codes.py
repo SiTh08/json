018e415df5a4f14525e454a652a49c140606c98e
Sivaharan Thevipagan <SThevipagan@spartaglobal.com>
2019-10-01 16:57:00 +0100

    added pseudocode for homework

--- package_requests_post_codes.py
+++ package_requests_post_codes.py
@@ -1,4 +1,5 @@
 import requests
+import _json
 
 request_bbc = requests.get('https://www.bbc.co.uk/news')
 
@@ -6,3 +7,13 @@ print(request_bbc)
 
 print(request_bbc.headers.keys())
 print(request_bbc.content)
+
+# Explore this request package that is installed.
+# Explore this http://postcodes.io/
+
+# Use requests to make a GET request to http://postcodes.io/ api
+# Search for a post code (can be your own or any)
+
+# save responses in variable
+
+# Use JSON library to unpack it
