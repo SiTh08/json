e6a2f77ac1b07d45b483039d0a5b378a3696d5f2
Sivaharan Thevipagan <SThevipagan@spartaglobal.com>
2019-10-01 15:35:43 +0100

    added readme and imported libraries and printed functions

---
+++ README.md
@@ -0,0 +1,4 @@
+# Libraries, Packages
+
+This class will cover what is in the title.
+
---
+++ py_libs.py
@@ -0,0 +1,29 @@
+# import random
+from random import randrange, random # Considered best practice.
+# from random import * # If you want all the methods inside random.
+import math
+import time
+
+print('///////////////////////RANDOM///////////////////////////')
+time.sleep(1)
+print(random())
+time.sleep(0.5)
+print(randrange(10, 44))
+
+# Libraries
+# They come built in
+# You just need to import.
+
+# They are maintained by python organisation
+# They are very stable and considered vanilla python.
+# check the documentation for more
+# https://docs.python.org/3/library/random.html
+
+
+num = 24.64
+
+print(math.ceil(num))
+time.sleep(0.31)
+print(math.floor(num))
+time.sleep(0.51)
+print(math.pi)
